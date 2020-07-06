fabd728acc72ce5509303340689a2331c662afdb
tnakagawa <nakagat@gmail.com>
2020-07-06 20:07:56 +0900

    update

--- demo1/service_worker.js
+++ demo1/service_worker.js
@@ -5,3 +5,12 @@ self.addEventListener('install', install);
 function install(event) {
     console.log(event);
 }
+
+self.addEventListener('fetch', function (event) {
+    console.log(event);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response ? response : fetch(event.request);
+        })
+    );
+});
