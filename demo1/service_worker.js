d8648eca90d8c969cdc81c9ceeed2a3610a2b001
tnakagawa <nakagat@gmail.com>
2020-07-07 08:41:56 +0900

    update

--- demo1/service_worker.js
+++ demo1/service_worker.js
@@ -1,16 +1,3 @@
-// TODO add event install
-
-self.addEventListener('install', install);
-
-function install(event) {
-    console.log(event);
-}
-
 self.addEventListener('fetch', function (event) {
     console.log(event);
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response ? response : fetch(event.request);
-        })
-    );
 });
