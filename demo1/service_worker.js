06141a93f07e2004c473c22920f539dc38cf29b7
tnakagawa <nakagat@gmail.com>
2020-07-08 10:33:06 +0900

    update

--- demo1/service_worker.js
+++ demo1/service_worker.js
@@ -1,3 +1,37 @@
-self.addEventListener('fetch', function (event) {
-    console.log(event);
-});
+var CACHE_NAME = 'demo1';
+var urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+];
+
+self.addEventListener('install', install);
+
+function install(event) {
+    console.log('install', event);
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log(cache);
+            return cache.addAll(urlsToCache);
+        })
+    );
+}
+
+self.addEventListener('fetch', fetch_);
+
+function fetch_(event) {
+    console.log('fetch', event, event.request.cache);
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
+        return;
+    console.log('event.request', event.request);
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                console.log('response', response);
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+}
