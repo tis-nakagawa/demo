52ec99afd34f994a3c33289d11b62fe24df14988
nakagawa <nakagawa.takatoshi@tis.co.jp>
2020-07-09 11:51:08 +0900

    update

--- demo1/service_worker.js
+++ demo1/service_worker.js
@@ -7,32 +7,35 @@ var urlsToCache = [
 self.addEventListener('install', install_);
 
 function install_(event) {
-    console.log('[Service Worker] install', event);
+    console.log('[Service Worker] install :', event);
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log(cache);
-            return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('[Service Worker] install - Opened cache :', cache);
+                return cache.addAll(urlsToCache);
+            })
     );
-}
-
-self.addEventListener('fetch', fetch_);
-
-function fetch_(event) {
-    console.log('[Service Worker] Fetched resource ' + event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            console.log('response', response);
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        }));
 }
 
 self.addEventListener('activate', activate_);
 
 function activate_(event) {
-    console.log('active', event);
+    console.log('[Service Worker] active :', event);
 }
 
+self.addEventListener('fetch', fetch_);
+
+function fetch_(event) {
+    console.log(self);
+    console.log('[Service Worker] fetch :' + event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                console.log('[Service Worker] fetch - response :', response);
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+}
